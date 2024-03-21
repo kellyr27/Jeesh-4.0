1529c0669b98299f20962552b62c9bf526276e80
Kelly <83458976+kellyr27@users.noreply.github.com>
2024-03-21 17:36:47 +1100

    Testing Soldier Movement
    Performing tests for Soldier movement.

--- src/scenes/GameScene/components/Soldier.js
+++ src/scenes/GameScene/components/Soldier.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import { Cone } from '@react-three/drei'
+import { useFrame } from 'react-three-fiber'
 
 const Soldier = ({ position, rotation, color }) => {
     
+    // Print out the position and rotation of the soldier every frame
+    useFrame(() => {
+        console.log('Soldiers position is ', position)
+    })
+
+
     return (
         <Cone 
             args={[0.4, 0.8]} 
---
+++ src/utils/arrayHelpers.js
@@ -0,0 +1,22 @@
+// Subtracts arr2 from arr1 and returns the result
+const subtractArrays = (arr1, arr2) => {
+    if (arr1.length !== arr2.length) {
+        return console.error('Arrays must be of the same length')
+    }
+
+    return arr1.map((el, index) => {
+        return el - arr2[index]
+    })
+}
+
+const addArrays = (arr1, arr2) => {
+    if (arr1.length !== arr2.length) {
+        return console.error('Arrays must be of the same length')
+    }
+
+    return arr1.map((el, index) => {
+        return el + arr2[index]
+    })
+}
+
+export { subtractArrays, addArrays }
